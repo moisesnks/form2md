b7ecfb8ba4d005f6c9707ae7ef0726ef20502580
moisesnks <mleiva@utem.cl>
2024-04-23 19:51:49 -0400

    gh-pages add

--- src/IssuePreview/index.jsx
+++ src/IssuePreview/index.jsx
@@ -4,13 +4,12 @@ import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import './styles.scss';
 
+
 export const parseToMarkdown = (submittedIssue) => {
     const { tag, title, description, tasks, observations, dueDate } = submittedIssue;
 
-    // Parsear el título a Markdown
     // Capitalizar tag
     const capitalizeTag = tag.charAt(0).toUpperCase() + tag.slice(1);
-    const markdownTitle = `# (${capitalizeTag}) ${title}`;
 
     // Parsear la descripción a Markdown
     const markdownDescription = `## Descripción\n${description}`;
@@ -21,11 +20,13 @@ export const parseToMarkdown = (submittedIssue) => {
     // Parsear las observaciones a Markdown
     const markdownObservations = `> ${observations}`;
 
-    // Parsear la fecha de vencimiento a Markdown
-    const markdownDueDate = `## Fecha de Vencimiento\n Se ha definido la fecha de entrega para el día ${dueDate}`;
+    // Duedate es un string en formato yyyy-mm-dd, debemos invertirlo para que quede dd-mm-yyyy
+    const dueDateParts = dueDate.split('-');
+    const dueDateFormatted = dueDateParts.reverse().join('-');
+    const markdownDueDate = `## Fecha de Vencimiento\nSe ha definido la fecha de entrega para el día ${dueDateFormatted}`;
 
     // Unir todas las partes en un solo string de Markdown
-    const markdownContent = `${markdownTitle}\n\n${markdownDescription}\n\n## Tareas\n${markdownTasks}\n\n${markdownObservations}\n\n${markdownDueDate}`;
+    const markdownContent = `${markdownDescription}\n\n## Tareas\n${markdownTasks}\n\n${markdownObservations}\n\n${markdownDueDate}`;
 
     return markdownContent;
 };
@@ -47,26 +48,44 @@ const Modal = ({ message, onClose }) => {
 
 export const IssuePreview = ({ submittedIssue }) => {
     const [showModal, setShowModal] = useState(false);
+    const [message, setMessage] = useState('');
 
-    const handleCopyMarkdown = () => {
-        navigator.clipboard.writeText(parseToMarkdown(submittedIssue))
-            .then(() => {
-                setShowModal(true);
-            })
-            .catch(error => console.error('Error al copiar al portapapeles: ', error));
-    };
+    const title = `(${submittedIssue.tag}) ${submittedIssue.title}`
 
     const closeModal = () => {
         setShowModal(false);
     };
 
+    const handleCopyTitle = () => {
+        navigator.clipboard.writeText(`(${submittedIssue.tag}) ${submittedIssue.title}`)
+            .then(() => {
+                setShowModal(true);
+                setMessage('Título copiado al portapapeles');
+            })
+            .catch(error => console.error('Error al copiar al portapapeles: ', error));
+    };
+
+    const handleCopyMarkdown = () => {
+        navigator.clipboard.writeText(parseToMarkdown(submittedIssue))
+            .then(() => {
+                setShowModal(true);
+                setMessage('Markdown copiado al portapapeles');
+            })
+            .catch(error => console.error('Error al copiar al portapapeles: ', error));
+    };
+
     return (
+
         <div className='md-container'>
+            <div className="md-title">
+                <h1> {title} </h1>
+                <button onClick={handleCopyTitle}>Copiar Título</button>
+            </div>
             <ReactMarkdown className="issue-preview" remarkPlugins={[gfm]}>
                 {parseToMarkdown(submittedIssue)}
             </ReactMarkdown>
             <button onClick={handleCopyMarkdown}>Copiar Markdown</button>
-            {showModal && <Modal message="Markdown copiado al portapapeles" onClose={closeModal} />}
+            {showModal && <Modal message={message} onClose={closeModal} />}
         </div>
     );
 };
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/form2md/',
   plugins: [react()],
 })
